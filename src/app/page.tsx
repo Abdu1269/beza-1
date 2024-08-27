79b6d16fa7cdf5494ade03e8ce0a3ee588434b51
Abdu1269 <50406895+Abdu1269@users.noreply.github.com>
2024-08-27 11:39:24 +0300

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+'use client'
+import React from 'react'
+
+import ProductPage from './products/page'
+
+import About from './about/page'
+import FAQ from '@/Components/FAQ'
+import WhyUs from '@/Components/whyUs'
+import Blogs from '@/Components/Blog'
+import SocialMedias from '@/Components/SocialMedias'
+import HomePage from './home/page'
+import { SWRConfig } from 'swr'
+import swrConfig from '@/shared/swrConfig'
+import Services from '@/Components/Services'
+import ProductListPage from '@/Components/Products'
+
+
+function page() {
+  return (
+    <div className='bg-white'>
+      <SWRConfig value={swrConfig}>
+        <HomePage />
+        <Services/>
+      <ProductListPage/>
+      
+      <WhyUs/>
+      <FAQ />
+      <Blogs/>
+      <About />
+      </SWRConfig>
+      
+    </div>
+  )
+}
+
+export default page
